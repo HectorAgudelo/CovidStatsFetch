a12c838ffe73fb93b862f790376a56b87185f0b4
Hector Agudelo <haacny86@gmail.com>
2021-04-19 15:58:33 -0400

    created another fuction holding the html file with results to improved order

--- js/app.js
+++ js/app.js
@@ -28,43 +28,47 @@ const countryStats = (countries) => {
       return response.json();
     })
     .then((body) => {
-      let { confirmed, country, critical, deaths, recovered } = body[0];
-      let resultsBody = 
+      countryResults(body)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const countryResults = (body)=>{
+    let { confirmed, country, critical, deaths, recovered } = body[0];
+    let resultsBody = 
 `<table class="table table-success table-striped">
-  <thead>
-    
-      <h1 class="text">${country}</h1>
-    
-  </thead>
-  <tbody>
-    <tr>
-      <th class="text" scope="row">Confirmed</th>
-      <td class="text" colspan="">${confirmed.toLocaleString()}</td>
-    </tr>
-    <tr>
-      <th class="text" scope="row">Critical</th>
-      <td class="text" colspan="2">${critical.toLocaleString()}</td>
-    </tr>
-    <tr>
-      <th class="text" scope="row">Deaths</th>
-      <td class="text" colspan="2">${deaths.toLocaleString()}</td>
-    </tr>
-    <tr>
-    <th class="text" scope="row">Recovered</th>
-    <td class="text" colspan="2">${recovered.toLocaleString()}</td>
-    </tr>
-  </tbody>
+<thead>
+  
+    <h1 class="text">${country}</h1>
+  
+</thead>
+<tbody>
+  <tr>
+    <th class="text" scope="row">Confirmed</th>
+    <td class="text" colspan="">${confirmed.toLocaleString()}</td>
+  </tr>
+  <tr>
+    <th class="text" scope="row">Critical</th>
+    <td class="text" colspan="2">${critical.toLocaleString()}</td>
+  </tr>
+  <tr>
+    <th class="text" scope="row">Deaths</th>
+    <td class="text" colspan="2">${deaths.toLocaleString()}</td>
+  </tr>
+  <tr>
+  <th class="text" scope="row">Recovered</th>
+  <td class="text" colspan="2">${recovered.toLocaleString()}</td>
+  </tr>
+</tbody>
 </table>
 <div class="btn">
-  <a href="/index.html" class="btn btn-success">Home</a>
+<a href="/index.html" class="btn btn-success">Home</a>
 </div>`;
 document.querySelector("body").innerHTML = ""
 let div = document.createElement("DIV")
 div.innerHTML=resultsBody
 document.querySelector("body").appendChild(div);
 
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+}
